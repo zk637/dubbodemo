8402a8d68f8d7e11ffe83b6a28d5c99c7c9ccc61
zk <zh1367065@gmail.com>
2021-02-03 20:15:33 +0800

    提交到hub

--- dubbodemo_provider/src/test/java/Test.java
+++ dubbodemo_provider/src/test/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("11111master");
         System.out.println("11111Test");
+        System.out.println("push");
     }
 }
